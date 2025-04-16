13802374c1f519e3b08365e0882ec44706e90582
Sam Joy <samjoy@Sams-Mac-mini.local>
2025-04-16 14:41:56 +0100

    completed first set of tests

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,9 +1,15 @@
 const db = require("../../db/connection");
 
+
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
-
-
+exports.createRef = (data) => {
+  const result = {};
+  data.forEach((article) => {
+    result[article.title] = article.article_id;
+  });
+  return result;
+};
