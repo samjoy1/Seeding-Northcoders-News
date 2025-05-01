73a6bae3979844824adf1afcd18606040c2edea1
Sam Joy <samjoy@Sams-Mac-mini.local>
2025-05-01 10:30:48 +0100

    completed task 8-delete

--- app/news.model.js
+++ app/news.model.js
@@ -70,3 +70,18 @@ exports.updateArticleById = (article_id, inc_votes) => {
         return result.rows[0]
     })
 }
+
+exports.removeCommentById = (comment_id) => {
+    if (isNaN(comment_id)) {
+        return Promise.reject({ status: 400, msg: "Invalid input"})
+    }
+
+    return db.query(
+        `DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id]
+    )
+    .then((result) => {
+        if (result.rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "Comment not found"})
+        }
+    })
+}
