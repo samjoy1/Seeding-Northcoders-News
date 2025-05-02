75df19210a8c4ffc3a0f589547c639fae254ebda
Sam Joy <samjoy@Sams-Mac-mini.local>
2025-05-02 16:19:55 +0100

    hosting changes

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,12 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set")
+} 
+
 const config = {}
 
 if (ENV === "production") {
@@ -9,15 +15,8 @@ if (ENV === "production") {
     config.max = 2
 }
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+
 
 const db = new Pool(config);
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("No PGDATABASE or DATABASE_URL set")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
-
 
 module.exports = db;
