ca6a976e14c0e78a4c8022ea3d204d67e1116964
Sam Joy <samjoy@Sams-Mac-mini.local>
2025-05-19 10:54:35 +0100

    connection js change

--- db/connection.js
+++ db/connection.js
@@ -11,8 +11,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 const config = {}
 
 if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.max = 2
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
 }
 
 
